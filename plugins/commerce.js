3d45b9b7686e4e4c5fdb40813bd21ce57b7e2c7d
NicholasGati <ngati016@hotmail.com>
2020-10-06 19:03:50 -0400

    Debugging

--- plugins/commerce.js
+++ plugins/commerce.js
@@ -1,5 +1,7 @@
 import Commerce from '@chec/commerce.js'
 const commerce = new Commerce(process.env.NUXT_ENV_CHEC_PUBLIC_API_KEY, true)
+// eslint-disable-next-line
+console.log(process.env.NUXT_ENV_CHEC_PUBLIC_API_KEY)
 export default (_, inject) => {
   inject('commerce', commerce)
 }
